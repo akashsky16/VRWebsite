83820a45faf39df7b9077d984188233883b2fb9f
akashsky16 <akash.singh1696@gmail.com>
2021-11-22 19:04:18 +0530

    First Commit

---
+++ app.js
@@ -0,0 +1,10 @@
+const slide2 = () => {
+    document.getElementById('text').innerHTML="Yes, Another World! No, Not Mars! <br> A World within the living world";
+    document.getElementById('singer').innerHTML="The Virtual World";
+    const img = document.getElementById('img2');
+    img.src ='./images/3 (1).png';
+}
+
+const slide1 = () => {
+    window.location.reload();
+}
